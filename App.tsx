e993df0d8148202ee72d8632a5267a34e058c4cd
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-08-14 13:47:13 -0300

    updating app to test screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import Toast from 'react-native-toast-message';
 import { useMatricula } from './src/hooks/useMatricula';
 import { ScannerScreen } from './src/screens/ScannerScreen';
 
-const App = () => {
-  const { matricula, saveMatricula, isLoading } = useMatricula();
+const LoginScreen = ({ onLogin }: { onLogin: (matricula: string) => void }) => {
   const [inputMatricula, setInputMatricula] = useState('');
-
-  if (isLoading) {
-    return <View style={styles.container}><ActivityIndicator size="large" /></View>;
-  }
-
-  if (matricula) {
-    return <ScannerScreen matricula={matricula} />;
-  }
-  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Digite sua Matrícula</Text>
@@ -28,37 +19,57 @@ const App = () => {
       <Button
         title="Salvar e Entrar"
         onPress={() => {
-            if(inputMatricula.trim()){
-                saveMatricula(inputMatricula.trim());
-            }
+          if (inputMatricula.trim()) {
+            onLogin(inputMatricula.trim());
+          }
         }}
       />
     </View>
   );
 };
 
+const App = () => {
+  const { matricula, saveMatricula, isLoading } = useMatricula();
+
+  if (isLoading) {
+    return <View style={styles.container}><ActivityIndicator size="large" /></View>;
+  }
+
+  return (
+    <>
+      {matricula ? (
+        <ScannerScreen matricula={matricula} />
+      ) : (
+        <LoginScreen onLogin={saveMatricula} />
+      )}
+      
+      <Toast />
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        justifyContent: 'center', 
-        padding: 20,
-        backgroundColor: '#f0f0f0'
-    },
-    title: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 20
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        backgroundColor: 'white',
-        padding: 12,
-        borderRadius: 8,
-        fontSize: 16,
-        marginBottom: 20
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f0f0f0'
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: 'white',
+    padding: 12,
+    borderRadius: 8,
+    fontSize: 16,
+    marginBottom: 20
+  }
+});
 
 export default App;
